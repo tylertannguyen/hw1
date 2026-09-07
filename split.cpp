f2521300125c2d02928db631a3b7bae068d16258
Tyler Nguyen <tylernguyen075@gmail.com>
2026-09-07 16:52:54 -0700

    Completed part 1 recursive split function

--- split.cpp
+++ split.cpp
@@ -17,7 +17,30 @@ the function below should be the only one in this file.
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
-// WRITE YOUR CODE HERE
+  // WRITE YOUR CODE HERE
+  // Base case: if the input list is empty, return
+  if (in == nullptr){
+    return;
+  }
+  // Saves current node to a temporary variable
+  Node* current = in;
+  // Move the input list pointer to the next node
+  in = in->next; 
+  // Recursive case: check if the current node's value is odd or even
+  // Recursively split list
+  split(in, odds, evens);
+  if (current->value % 2 == 0){
+    // If value is even, add it to evens list
+    current->next = evens;
+    // Update the evens head pointer to the current node
+    evens = current;
+  } else {
+    // If value is odd, add it to odds list
+    current->next = odds;
+    // Update the odds head pointer to current node
+    odds = current;
+  }
 }
+
 
 /* If you needed a helper function, write it here */
