2fc88035c0b95c0c25c26382889fdedc193bc55e
Tyler Nguyen <tylernguyen075@gmail.com>
2026-09-07 17:51:51 -0700

    Add test for recursive split

--- test_split.cpp
+++ test_split.cpp
@@ -10,8 +10,47 @@ g++ split.cpp test_split.cpp -o test_split
 */
 
 #include "split.h"
+#include <iostream>
+
+void printList(Node* head)
+{
+  while (head != nullptr) {
+    std::cout << head->value << " ";
+    head = head->next;
+  }
+  std::cout << "\n";
+}
+
+void deleteList(Node* head)
+{
+  while (head != nullptr) {
+    Node* next = head->next;
+    delete head;
+    head = next;
+  }
+}
 
 int main(int argc, char* argv[])
 {
+  // Create the sorted list: 1 -> 2 -> 3 -> 4
+  Node* input = new Node(1, nullptr);
+  input->next = new Node(2, nullptr);
+  input->next->next = new Node(3, nullptr);
+  input->next->next->next = new Node(4, nullptr);
 
+  Node* odds = nullptr;
+  Node* evens = nullptr;
+
+  split(input, odds, evens);
+
+  std::cout << "Odds: ";
+  printList(odds);
+
+  std::cout << "Evens: ";
+  printList(evens);
+
+  deleteList(odds);
+  deleteList(evens);
+
+  return 0;
 }
